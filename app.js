934bfe4ae0fb64effb8021ea9bb5b6a8b279768b
Thayazeredo <azeredothaylini@gmail.com>
2025-04-11 13:08:33 -0300

    Update 2025-04-09

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const sqlite3 = require("sqlite3");
 
@@ -14,7 +15,13 @@ db.serialize(() => {
   );
 });
 
+// Aqui sera acrescentado uma rota "/static", para a pasta __dirname + "/static"
+// O app.use é usado para acrescentar rotas novas para o Express gerenciar e pode usar
+// Middleware para isto, que neste caso é o express.static, que grencia rotas estaticas
 app.use("/static", express.static(__dirname + "/static"));
+
+// Middleware para processar as requisições do Body Parameters dp cliente
+app.use(bodyParser.urlencoded({ extented: true }));
 
 // Configurar EJS como o motor de visualização
 app.set("view engine", "ejs");
@@ -27,29 +34,85 @@ const sobre = 'Voce esta na pagina "sobre"<br><a href="/">sobre</a>';
 const Login = 'Voce esta na pagina "Login"<br><a href="/">Login</a>';
 const Cadastro = 'Voce esta na pagina "Cadastro"<br><a href="/">Cadastro</a>';
 
+/* Método express.get necessita de dois parametros
+Na ARROW FUNCTION, o primeiro são os dados do servidor (REQUISITION - `req`)
+o segundo, são os dados que serão enviados ao cliente (RESULT - `res`)*/
+
 app.get("/", (req, res) => {
+  console.log("GET /index");
   // res.send(home);
+  // res.redirect("/cadastro"); // Redireciona para a ROTA cadastro
   res.render("index");
 });
 
+app.get("/usuarios", (req, res) => {
+  const query = "SELECT * FROM users";
+  db.all(query, (err, row) => {
+    console.log(`GET /usuarios ${JSON.stringify(row)}`);
+    res.send("Lista de usuarios.");
+  });
+});
+
 app.get("/sobre", (req, res) => {
-  res.send(sobre);
+  console.log("GET /sobre");
+  res.render("sobre");
 });
 
 app.get("/login", (req, res) => {
-  res.render(login);
+  console.log("GET /login");
+  // Rota raiz  do meu servidor
+  res.render("login");
 });
 
 app.post("/login", (req, res) => {
-  res.send("Login ainda não implementado.");
+  console.log("POST /login");
+  res.render("Login ainda não implementado.");
 });
 
 app.get("/cadastro", (req, res) => {
-  res.send(Cadastro);
+  console.log("GET /cadastro");
+  res.render("cadastro");
 });
 
-// app.listen() deve ser o ultimo comando da aplicação (app.js)
+app.post("/cadastro", (req, res) => {
+  !req.body
+    ? console.log(JSON.stringify(req.body))
+    : console.log(`Body vazio: ${req.body}`);
+
+  const { username, password, email, celular, cpf, rg } = req.body;
+  // Colocar aqui as validações e inclusão no banco de dados do cadastro do usuario
+  // 1. Validar dados do usuario
+  // 2. saber se ele ja existe no banco
+  const query =
+    "SELECT * FROM users WHERE email=? OR cpf=? OR rg=? OR usernmae=?";
+  db.get(query, [email, cpf, rg, username], (err, row) => {
+    if (err) throw err;
+
+    if (row) {
+      // A variavel `row` irá retornar os dados do banco de dados,
+      // executado atraves do SQL, variavel query
+      res.send("Usuario já cadastrado, refaça o cadastro");
+    } else {
+      // 3. Se usuario não existe no banco cadastrar
+      const insertQuery =
+        "INSERT INTO users (username, password, email, celular, cpf, rg) VALUES (?,?,?,?,?,?)";
+      db.ruin(
+        insertQuery,
+        [username, password, email, celular, cpf, rg],
+        (err) => {
+          // Inserir a logica do INSERT
+          res.send("Usuario cadastrado com sucesso");
+        }
+      );
+    }
+  });
+
+  console.log("POST /cadastro");
+  res.send(
+    `Bem vindo usuario: ${req.body.username}, seu email é ${req.body.email}`
+  );
+});
+//app.listen() deve ser o último comando da aplicação (app.js)
 app.listen(PORT, () => {
   console.log(`Servidor sendo executado na porta ${PORT}!`);
 });
-//teste
